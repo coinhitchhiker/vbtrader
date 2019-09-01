2ac58d49e899a92d5c3c86b56db8c489a54521c7
Nochul Park <maceo.park@gmail.com>
2019-08-31 22:42:10 -0700

    human readable candle file name

--- simulator/src/main/java/com/coinhitchhiker/vbtrader/simulator/SimulatorRepositoryImpl.java
+++ simulator/src/main/java/com/coinhitchhiker/vbtrader/simulator/SimulatorRepositoryImpl.java
@@ -8,6 +8,8 @@ import com.coinhitchhiker.vbtrader.simulator.db.SimulatorDAO;
 import com.google.gson.Gson;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -148,7 +150,11 @@ public class SimulatorRepositoryImpl implements Repository {
     }
 
     private String makeFileName(String exchange, String symbol, long simulStart, long simulEnd, boolean REPO_USE_DB) {
-        return exchange + "-" + symbol + "-" + simulStart + "-" + simulEnd + (REPO_USE_DB ? "_DB" : "");
+        DateTimeFormatter dtfOut = DateTimeFormat.forPattern("yyyyMMdd");
+
+        String s = dtfOut.print(new DateTime(simulStart, UTC));
+        String e = dtfOut.print(new DateTime(simulEnd, UTC));
+        return exchange + "-" + symbol + "-" + s + "-" + e + (REPO_USE_DB ? "_DB" : "");
     }
 
     private void serCandles(List<Candle> candles, String filename) {
